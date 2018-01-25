b1098a66a8a3203af1e126471022705b482c8bf5
Derek Haller <dhaller.personal@gmail.com>
2018-01-25 04:01:06 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,11 +4,14 @@
 # HUH?! SPEAK UP, SONNY!
 def speak_to_grandma(phrase)
   shout = phrase.upcase
-  whisper = phrase.downcase
   special == 'I LOVE YOU GRANDMA!'
   if phrase == shout
     puts "NO, NOT SINCE 1938!"
-  elsif phrase == whisper
+  elsif phrase == special
+    puts  'I LOVE YOU TOO PUMPKIN!'
+  else
+    puts 'HUH?! SPEAK UP, SONNY!'
+end
     
     
     
